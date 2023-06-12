2b0c56c95194f4c432e34d472ccb893b0316d8fd
CrativeMan <hannig.sebi@gmail.com>
2023-06-12 22:20:00 +0200

    Added READme file and label that will display time

--- water_timer.py
+++ water_timer.py
@@ -8,9 +8,26 @@ class WaterTimer(ctk.CTk):
         self.title(title)
         self.geometry(f"{size[0]}x{size[1]}")
 
-        ctk.CTkLabel(self, text="Test").pack(expand=True, fill=tk.BOTH)
+        # create the widgets
+        WaterTimerFrame(self).pack()
 
+        # run the mainloop
         self.mainloop()
+
+
+class WaterTimerFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        # * layout
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure((0, 1, 2, 3), weight=1, uniform=1)
+
+        # * widgets
+        self.timer = ctk.CTkLabel(
+            self, text="00:00:00", font=("Arial", 50), text_color="red"
+        )
+        self.timer.grid(row=0, rowspan=2, column=0, sticky="nsew")
 
 
 app = WaterTimer("Water Timer", (500, 500))

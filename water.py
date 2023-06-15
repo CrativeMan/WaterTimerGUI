5472aa5b93c3a58d3214e667d3f41fbcd335171a
CrativeMan <hannig.sebi@gmail.com>
2023-06-15 21:54:48 +0200

    Added entire layout

--- water.py
+++ water.py
@@ -1,6 +1,5 @@
 import customtkinter as ctk
 from settings import *
-from water_calc_functions import *
 import time
 
 class WaterTimer(ctk.CTk):
@@ -16,23 +15,32 @@ class WaterTimer(ctk.CTk):
             FONT["timer-font"]["weight"],
         )
 
+        self.main_font = (
+            FONT["main-font"]["family"],
+            FONT["main-font"]["size"],
+            FONT["main-font"]["weight"],
+        )
+
+
         # * widgets
         Widgets(
             parent=self, 
             timer_font=self.timer_font,
+            main_font=self.main_font,
         ).pack(expand=True, fill=ctk.BOTH)
 
         # * run
         self.mainloop()
 
 class Widgets(ctk.CTkFrame):
-    def __init__(self, parent, timer_font):
+    def __init__(self, parent, timer_font, main_font):
         super().__init__(parent)
         # * layout
         self.rowconfigure((0,1,2,3),  weight=1, uniform=1)
-        self.columnconfigure(0, weight=1, uniform=1)
+        self.columnconfigure((0,1,2), weight=1, uniform=1)
 
         # * widgets
+        # timer label
         self.timer_label = ctk.CTkLabel(
             self,
             text="00:00:00",
@@ -44,9 +52,94 @@ class Widgets(ctk.CTkFrame):
             row=0, 
             rowspan=2, 
             column=0, 
+            columnspan=3,
             sticky="nsew",
             padx=STYLING["Gap"],
             pady=STYLING["Gap"]
         )
 
-water_timer = WaterTimer("Water timer", (500,400))
+        # add/subtract time label
+        self.add_subtract_time_label = ctk.CTkLabel(
+            self,
+            text="00:00:00",
+            fg_color=COLORS["dark-grey"]["fg"],
+            font=main_font,
+            corner_radius=STYLING["CornerRadius"],
+        )
+        self.add_subtract_time_label.grid(
+            row=2,
+            column=1,
+            sticky="nsew",
+            padx=STYLING["Gap"],
+            pady=STYLING["Gap"],
+        )
+
+        # add time button
+        self.add_time_button = ctk.CTkButton(
+            self,
+            text="+1 min",
+            fg_color=COLORS["orange"]["fg"],
+            hover_color=COLORS["orange"]["hover"],
+            font=main_font,
+        )
+        self.add_time_button.grid(
+            row=2, 
+            column=0, 
+            sticky="nsew", 
+            padx=STYLING["Gap"], 
+            pady=STYLING["Gap"]
+        )
+
+        # subtract time button
+        self.subtract_time_button = ctk.CTkButton(
+            self,
+            text="-1 min",
+            fg_color=COLORS["orange"]["fg"],
+            hover_color=COLORS["orange"]["hover"],
+            font=main_font,
+            corner_radius=STYLING["CornerRadius"],
+        )
+        self.subtract_time_button.grid(
+            row=2,
+            column=2,
+            sticky="nsew",
+            padx=STYLING["Gap"],
+            pady=STYLING["Gap"],
+        )
+
+        # start button
+        self.start_button = ctk.CTkButton(
+            self,
+            text="Start",
+            fg_color=COLORS["orange"]["fg"],
+            hover_color=COLORS["orange"]["hover"],
+            font=main_font,
+            corner_radius=STYLING["CornerRadius"],
+        )
+        self.start_button.grid(
+            row=3,
+            column=0,
+            columnspan=2,
+            sticky="nsew",
+            padx=STYLING["Gap"],
+            pady=STYLING["Gap"],
+        )
+
+        # stop button
+        self.stop_button = ctk.CTkButton(
+            self,
+            text="Stop",
+            fg_color=COLORS["orange"]["fg"],
+            hover_color=COLORS["orange"]["hover"],
+            font=main_font,
+            corner_radius=STYLING["CornerRadius"],
+        )
+        self.stop_button.grid(
+            row=3,
+            column=2,
+            sticky="nsew",
+            padx=STYLING["Gap"],
+            pady=STYLING["Gap"],
+        )
+
+water_timer = WaterTimer("Water timer", (500,500))

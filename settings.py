d3edde0d0643e068a2241551bc44c4e838eb5266
CrativeMan <hannig.sebi@gmail.com>
2023-06-15 21:53:10 +0200

    Added the entire layout

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 COLORS = {
     "orange": {"fg": "#e63b10", "hover": "#b32c14"},
-    "dark-red": {"fg": "#880b0b", "hover": "#631414","text": "#880b0b", "disabled":"#631414",},
     "dark-grey": {"fg": "#1b1818"},
 }
 
@@ -13,6 +12,11 @@ FONT = {
     "timer-font": {
         "family": "Helvetica",
         "size": 40,
+        "weight": "bold",
+    },
+    "main-font": {
+        "family": "Helvetica",
+        "size": 20,
         "weight": "bold",
     }
 }
